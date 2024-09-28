9b9fbaa6b6eb216e222b5d1daa40cbd703cf987d
BeygonK <kipkoechbeygon@gmail.com>
2024-09-28 08:55:24 +0300

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,51 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+      console.error(`Error connecting to Redis: ${err}`);
+    });
+
+    this.client.on('connect', () => {
+      console.log('Connected to Redis');
+    });
+  }
+
+  isAlive() {
+    return this.client.isOpen();
+  }
+
+  async get(key) {
+    try {
+      const value = await this.client.get(key);
+      return value;
+    } catch (err) {
+      console.error(`Error getting key ${key}: ${err}`);
+      return null;
+    }
+  }
+
+  async set(key, value, duration) {
+    try {
+      await this.client.set(key, value, {
+        EX: duration,
+      });
+    } catch (err) {
+      console.error(`Error setting key ${key}: ${err}`);
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.client.del(key);
+    } catch (err) {
+      console.error(`Error deleting key ${key}: ${err}`);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
