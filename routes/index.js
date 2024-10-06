155d941b57314103c36d7773488e8541992d2e41
BeygonK <kipkoechbeygon@gmail.com>
2024-10-06 21:21:56 +0300

    Routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -12,5 +14,17 @@ router.get('/stats', AppController.getStats);
 
 // POST users to /users
 router.post('/users', UsersController.postNew);
+
+// GET /connect
+router.get('/connect', AuthController.getConnect);
+
+// GET /disconnect
+router.get('/disconnect', AuthController.getDisconnect);
+
+// GET /users/me
+router.get('/users/me', UsersController.getMe);
+
+// POST /files
+router.post('/files', FilesController.postUpload);
 
 export default router;
